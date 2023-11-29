514f135d2a08c949a3aa20b4843e21c0061b3eb6
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-29 14:03:15 +0100

    Add global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*,
+ ::before,
+ ::after {
+   box-sizing: border-box;
+  }
+
+  body,
+  h1, h2 {
+   margin: 0;
+  }
+
+  html {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  ul {
+   list-style: none;
+   margin: 0;
+   padding: 0;
+  }
+
+  img {
+   width: 100%;
+  }
+
+  a {
+   text-decoration: none;
+   color: inherit;
+  }     
+
+  button {
+   border: none;
+   background-color: transparent;
+   font: inherit;
+   cursor: pointer;  
+  }
+`;
+
+export default GlobalStyle;
