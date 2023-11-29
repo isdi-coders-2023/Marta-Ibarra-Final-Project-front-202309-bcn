d65b2b4d538c80373ef6c814ac7ebd0442b80c88
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-29 18:33:36 +0100

    Add Header styles

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  padding: 20px;
+
+  .header-title {
+    color: #000;
+    font-family: Inter;
+    font-size: 32px;
+    font-style: italic;
+    font-weight: 200;
+    line-height: normal;
+    text-decoration-line: underline;
+    text-align: center;
+  }
+`;
+
+export default HeaderStyled;
