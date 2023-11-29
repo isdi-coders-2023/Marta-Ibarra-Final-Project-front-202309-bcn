371b9481e917c851752b83361ed95804f8491e12
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-29 21:49:55 +0100

    Add Nav component to App & header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
+import Navigation from "../Navigation/Navigation";
 import HeaderStyled from "./Header.styled";
 
 const Header = (): React.ReactElement => {
   return (
     <HeaderStyled>
       <h1 className="header-title">La Galerie</h1>
+      <Navigation />
     </HeaderStyled>
   );
 };
