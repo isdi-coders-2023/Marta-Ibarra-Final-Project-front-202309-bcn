2e5f523c30f92f572eae66f2fc7512922fa7bad8
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-29 18:35:37 +0100

    Add Header test

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyle";
 import App from "./components/App/App";
+import Header from "./components/Header/Header";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+        <Header />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
