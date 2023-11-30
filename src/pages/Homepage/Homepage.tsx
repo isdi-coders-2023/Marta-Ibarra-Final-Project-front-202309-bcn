4639b71ace4a4264fa458afde44dfa950ae73de8
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-30 21:07:46 +0100

    Add loadPaintings dispatcher

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,14 @@
+import { useDispatch } from "react-redux";
 import HomepageStyled from "./Homepage.styled";
+import paintingsMock from "../../testUtils/paintingsMock";
+import { loadPaintingsActionCreator } from "../../store/paintings/paintingsSlice";
 
 const Homepage = (): React.ReactElement => {
+  const dispatch = useDispatch();
+
+  const currentPaintings = paintingsMock;
+  dispatch(loadPaintingsActionCreator(currentPaintings));
+
   return (
     <HomepageStyled>
       <h2 className="section-title">Homepage</h2>
