5e9b39b4f84b358476af17a522611fa57100f3a1
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-01 20:13:43 +0100

    Add PaintingsList styles

---
+++ src/components/PaintingsList/PaintingsListStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PaintingsListStyled = styled.ul`
+  display: grid;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;
+
+export default PaintingsListStyled;
