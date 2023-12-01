3cec0fa10ebd023971f1bbeda9c86495ce050d81
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-01 10:15:49 +0100

    Fix dispatch in Homepage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,17 +1,18 @@
-import { useDispatch } from "react-redux";
 import HomepageStyled from "./Homepage.styled";
 import paintingsMock from "../../testUtils/paintingsMock";
 import { loadPaintingsActionCreator } from "../../store/paintings/paintingsSlice";
+import PaintingsList from "../../components/PaintingsList/PaintingsList";
+import { useAppDispatch } from "../../store/hooks";
 
 const Homepage = (): React.ReactElement => {
-  const dispatch = useDispatch();
-
+  const dispatch = useAppDispatch();
   const currentPaintings = paintingsMock;
   dispatch(loadPaintingsActionCreator(currentPaintings));
 
   return (
     <HomepageStyled>
       <h2 className="section-title">Homepage</h2>
+      <PaintingsList />
     </HomepageStyled>
   );
 };
