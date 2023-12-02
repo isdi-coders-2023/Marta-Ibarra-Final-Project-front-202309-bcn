3401814f4c9c694a45853e416f2a15ed5f445c72
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-02 22:00:30 +0100

    Add usePaintingsApi custom hook

---
+++ src/hooks/usePaintingsApi.tsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { PaintingStructure } from "../store/types";
+
+const usePaintingsApi = () => {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  const getPaintingsApi = useCallback(async () => {
+    const { data: paintings } = await axios.get<{
+      paintings: PaintingStructure[];
+    }>("/paintings");
+
+    return paintings;
+  }, []);
+
+  return { getPaintingsApi };
+};
+
+export default usePaintingsApi;
