96d131fda11d5b17b125d06d4c2ce93544988581
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-02 22:01:42 +0100

    Add custom hook test

---
+++ src/hooks/__tests__/usePaintingsApi.test.tsx
@@ -0,0 +1,21 @@
+import { renderHook } from "@testing-library/react";
+import paintingsMock from "../../mocks/paintingsMock";
+import usePaintingsApi from "../usePaintingsApi";
+
+describe("Given a usePaintingsApi custom hook", () => {
+  describe("When it receives the  information of 'Boys who swim', 'Untitled', and 'Stuck on you'", () => {
+    test("Then it should show 'Boys who swim', 'Untitled', and 'Stuck on you' information", async () => {
+      const expectedPaintings = paintingsMock;
+
+      const {
+        result: {
+          current: { getPaintingsApi },
+        },
+      } = renderHook(() => usePaintingsApi());
+
+      const currentPaintings = await getPaintingsApi();
+
+      expect(currentPaintings).toStrictEqual(expectedPaintings);
+    });
+  });
+});
