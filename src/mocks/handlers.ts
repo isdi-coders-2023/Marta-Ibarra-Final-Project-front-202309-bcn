42db6477156198a773a55d07b1a3e0996f832bf1
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-04 16:55:17 +0100

    Remove async handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import paintingsMock from "./paintingsMock";
 
 const urlApi = import.meta.env.VITE_API_URL;
 const handler = [
-  http.get(`${urlApi}/paintings`, async () => {
+  http.get(`${urlApi}/paintings`, () => {
     return HttpResponse.json({ paintings: paintingsMock });
   }),
 ];
