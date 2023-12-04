10f921f43350917120a26e7e25ff598c2abb8d97
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-04 17:01:38 +0100

    Fix imports in setuptests

--- src/hooks/usePaintingsApi.tsx
+++ src/hooks/usePaintingsApi.tsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import { useCallback } from "react";
 import { PaintingStructure } from "../store/types";
 
-const usePaintingsApi = () => {
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
+const usePaintingsApi = () => {
   const getPaintingsApi = useCallback(async () => {
     const { data: paintings } = await axios.get<{
       paintings: PaintingStructure[];
