1c0b54a6c28ba373901f0325d3330391f1bd5583
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-09 18:52:06 +0100

    Add addPainting custom hook test

--- src/hooks/__tests__/usePaintingsApi.test.tsx
+++ src/hooks/__tests__/usePaintingsApi.test.tsx
@@ -4,16 +4,16 @@ import usePaintingsApi from "../usePaintingsApi";
 import { providerWrapper } from "../../testUtils/customRender";
 
 describe("Given a usePaintingsApi custom hook", () => {
+  const expectedPaintings = { paintings: paintingsMock };
+
+  const {
+    result: {
+      current: { getPaintingsApi, addnewPainting, deletePainting },
+    },
+  } = renderHook(() => usePaintingsApi(), { wrapper: providerWrapper });
+
   describe("When it receives the  information of 'Boys who swim', 'Untitled', and 'Stuck on you'", () => {
     test("Then it should show 'Boys who swim', 'Untitled', and 'Stuck on you' information", async () => {
-      const expectedPaintings = { paintings: paintingsMock };
-
-      const {
-        result: {
-          current: { getPaintingsApi },
-        },
-      } = renderHook(() => usePaintingsApi(), { wrapper: providerWrapper });
-
       const currentPaintings = await getPaintingsApi();
 
       expect(currentPaintings).toStrictEqual(expectedPaintings);
@@ -25,15 +25,19 @@ describe("Given a usePaintingsApi custom hook", () => {
       const expectedDeletedPaintingId = paintingsMock[0]._id;
       const expectedResponse = {};
 
-      const {
-        result: {
-          current: { deletePainting },
-        },
-      } = renderHook(() => usePaintingsApi(), { wrapper: providerWrapper });
-
       const response = await deletePainting(expectedDeletedPaintingId);
 
       expect(response).toStrictEqual(expectedResponse);
+    });
+  });
+
+  describe("When it calls its addPaintingsApi method with 'Untitled' ", () => {
+    test("Then it should return 'Untitled' from the API", async () => {
+      const newPainting = paintingsMock[1];
+
+      const response = await addnewPainting(newPainting);
+
+      expect(response).toStrictEqual(newPainting);
     });
   });
 });
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,23 @@
 import { http, HttpResponse } from "msw";
-import { paintingsMock } from "./paintingsMock";
+import { jamelPaintingMock, paintingsMock } from "./paintingsMock";
 
 const urlApi = import.meta.env.VITE_API_URL;
 
 export const handlers = [
-  http.get(`${urlApi}/paintings`, () => {
-    return HttpResponse.json({ paintings: paintingsMock });
-  }),
-  http.delete(`${urlApi}/paintings/:_id`, () => {
-    return HttpResponse.json({});
-  }),
-  http.post(`${urlApi}/paintings/add`, () => {
-    return HttpResponse.json({ painting: paintingsMock });
-  }),
+  http.get(`${urlApi}/paintings`, () =>
+    HttpResponse.json({ paintings: paintingsMock }),
+  ),
+  http.delete(`${urlApi}/paintings/:_id`, () => HttpResponse.json({})),
+
+  http.post(`${urlApi}/paintings/add`, () =>
+    HttpResponse.json({ painting: jamelPaintingMock }),
+  ),
 ];
 
 export const errorHandlers = [
-  http.get(`${urlApi}/paintings`, async () => {
-    return HttpResponse.error();
-  }),
-  http.delete(`${urlApi}/paintings/:_id`, async () => {
-    return HttpResponse.error();
-  }),
+  http.get(`${urlApi}/paintings`, async () => HttpResponse.error()),
+
+  http.delete(`${urlApi}/paintings/:_id`, async () => HttpResponse.error()),
+
+  http.post(`${urlApi}/paintings/add`, () => HttpResponse.error()),
 ];
