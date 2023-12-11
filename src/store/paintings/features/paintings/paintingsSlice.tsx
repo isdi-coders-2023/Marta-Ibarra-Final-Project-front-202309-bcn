8ba35d88a66bf934c3e2d146a90b4d0e84f27403
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-11 21:11:36 +0100

    Add modify paintings reducer slice

--- src/store/paintings/features/paintings/paintingsSlice.tsx
+++ src/store/paintings/features/paintings/paintingsSlice.tsx
@@ -40,6 +40,15 @@ const paintingsSlice = createSlice({
       ...currentState,
       selectedPainting: action.payload,
     }),
+    modifyPainting: (
+      currentState,
+      action: PayloadAction<PaintingStructure>,
+    ) => ({
+      ...currentState,
+      paintings: currentState.paintings.map((painting) =>
+        painting._id !== action.payload._id ? painting : action.payload,
+      ),
+    }),
   },
 });
 
@@ -48,6 +57,7 @@ export const {
   deletePainting: deletePaintingActionCreator,
   addNewPainting: addNewPaintingActionCreator,
   loadSelectedPainting: loadSelectedPaintingActionCreator,
+  modifyPainting: modifyPaintingActionCreator,
 } = paintingsSlice.actions;
 
 export const paintingsReducer = paintingsSlice.reducer;
