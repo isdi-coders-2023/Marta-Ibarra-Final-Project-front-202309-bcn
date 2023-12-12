7ad144727725afd922026569edd86285d32bf2a6
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-13 00:10:53 +0100

    Fix footer content alignment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,22 +4,24 @@ const Footer = (): React.ReactElement => {
   return (
     <FooterStyled>
       <span>B-art inc, all rights reserved</span>
-      <a target="_blank" href="https://www.instagram.com">
-        <img
-          src="/images/instagram-icon.png"
-          alt="instagram logo with link"
-          width="23"
-          height="15"
-        />
-      </a>
-      <a target="_blank" href="https://www.twitter.com">
-        <img
-          src="/images/twitter-icon.png"
-          alt="twitter logo with link"
-          width="27"
-          height="17"
-        />
-      </a>
+      <div className="footer_icon">
+        <a target="_blank" href="https://www.instagram.com">
+          <img
+            src="/images/instagram-icon.png"
+            alt="instagram logo with link"
+            width="23"
+            height="15"
+          />
+        </a>
+        <a target="_blank" href="https://www.twitter.com">
+          <img
+            src="/images/twitter-icon.png"
+            alt="twitter logo with link"
+            width="27"
+            height="17"
+          />
+        </a>
+      </div>
     </FooterStyled>
   );
 };
