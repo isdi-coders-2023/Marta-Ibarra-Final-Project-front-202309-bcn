3226cee9f2fbd50823a3c74841cea6bc24a129e8
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-13 10:50:54 +0100

    Fix footer images

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,25 +3,7 @@ import FooterStyled from "./FooterStyled";
 const Footer = (): React.ReactElement => {
   return (
     <FooterStyled>
-      <span>B-art inc, all rights reserved</span>
-      <div className="footer_icon">
-        <a target="_blank" href="https://www.instagram.com">
-          <img
-            src="/images/instagram-icon.png"
-            alt="instagram logo with link"
-            width="23"
-            height="15"
-          />
-        </a>
-        <a target="_blank" href="https://www.twitter.com">
-          <img
-            src="/images/twitter-icon.png"
-            alt="twitter logo with link"
-            width="27"
-            height="17"
-          />
-        </a>
-      </div>
+      <span>B-art inc, all rights reserved ©</span>
     </FooterStyled>
   );
 };
